f09df10ab917a7640ba35bd0a9f9a7df2b5cb4ba
Danil Kuzmin <dazekamikadze19962002@gmail.com>
2019-04-26 18:13:47 +0300

    index desctop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,8 @@ $(document).ready(function () {
 		autoplaySpeed: 3000,
 		pauseOnHover: false,
 		draggable: false,
-		swipe: false
+		swipe: false,
+		lazyLoad: 'progressive'
 	});
 	var $status = $('.main-banner-current');
 	var $slickElement = $('.main-banner-slider');
@@ -26,7 +27,8 @@ $(document).ready(function () {
 	$('.main-tabs-tab-slider').filter(':first').slick({
 		slidesToShow: 3,
   	slidesToScroll: 1,
-		infinite: false
+		infinite: false,
+		lazyLoad: 'progressive'
 	});
 	$('.main-tabs-navigation__item').click(function (e) {
 		e.preventDefault();
@@ -36,7 +38,8 @@ $(document).ready(function () {
 		mainTabContainers.filter(this.hash).find('.main-tabs-tab-slider').slick({
 			slidesToShow: 3,
 			slidesToScroll: 1,
-			infinite: false
+			infinite: false,
+			lazyLoad: 'progressive'
 		});
 		$('.main-tabs-navigation__item').removeClass('selected');
 		$(this).addClass('selected');
@@ -46,8 +49,79 @@ $(document).ready(function () {
 		slidesToShow: 1,
 		variableWidth: true,
 		focusOnSelect: true,
-		draggable: false		
+		draggable: false,
+		lazyLoad: 'progressive'		
 	});
+
+	$(document).find('[name="tel"]').inputmask({
+		mask: "+7 (999) 999-99-99",
+		showMaskOnHover: false,
+		showMaskOnFocus: true
+	});
+	$(document).find('[name="email"]').inputmask({ 
+		alias: "email",
+		showMaskOnHover: false,
+		showMaskOnFocus: true
+	});
+
+	var mainFormContainers = $('.main-form-content-right-tabs-tab');
+	mainFormContainers.hide().filter(':first').fadeIn();
+	$('.main-form-content-right-tabs-navigation label').filter(':first').addClass('selected');
+	$('.main-form-content-right-tabs-navigation a').click(function (e) {
+		e.preventDefault();
+		mainFormContainers.hide();
+		mainFormContainers.filter(this.hash).fadeIn();
+		$('.main-form-content-right-tabs-navigation label').removeClass('selected');
+		$(this).find('label').addClass('selected');
+	});
+
+	let feed = new Instafeed({
+		get: 'user',
+		userId: '2215809475', /*https://codeofaninja.com/tools/find-instagram-user-id*/
+		accessToken: '2215809475.f3c5285.5061b5f75408449b917c36fb1c4fa89d', /*https://www.youtube.com/watch?time_continue=70&v=fGMk8daxF08*/
+		resolution: 'standard_resolution',
+		limit: 4,
+		useHttp: 'true',
+		template: '<a data-fancybox class="instafeed-item" data-src="{{link}}" href="javascript:;"><div class="border-1"></div><div class="border-2"></div><div class="border-3"></div><div class="border-4"></div><div class="instafeed-item-top"><div class="instafeed-item-top__logo"></div><div class="instafeed-item-top-info"><div class="instafeed-item-top-info__title">{{model.user.username}}</div><div class="instafeed-item-top-info__loc">{{location}}</div></div><div class="instafeed-item-top__more"></div></div><div class="instafeed-item-photo"><img src="{{image}}"/></div><div class="instafeed-item-bot"><div class="instafeed-item-bot-top"><div class="instafeed-item-bot-top-like"></div><div class="instafeed-item-bot-top-comment"></div><div class="instafeed-item-bot-top-share"></div><div class="instafeed-item-bot-top-fav"></div></div><div class="instafeed-item-bot-likes">{{likes}}<span> likes</span></div><div class="instafeed-item-bot-descr"><span>{{model.caption.from.username}}</span>{{caption}}</div><div class="instafeed-item-bot-dateHidden">{{model.created_time}}</div><div class="instafeed-item-bot-date"></div></div> </a>'
+	});
+	feed.run();
+
+	function timeConverter(UNIX_timestamp){
+		var a = new Date(UNIX_timestamp * 1000);
+		var months = ['Января','Февраля','Марта','Апреля','Мая','Июня','Июля','Августа','Сентября','Октября','Ноября','Декабря'];
+		var year = a.getFullYear();
+		var month = months[a.getMonth()];
+		var date = a.getDate();
+		var time = date + ' ' + month + ' ' + year;
+		return time;
+	}
+
+	var initialSlick = function(allmutations){
+    // allmutations — массив, и мы можем использовать соответствующие методы JavaScript.
+    allmutations.map( function(mr){
+			if( !$(document).find('#instafeed').hasClass('slick-initialized') ){
+				$(document).find('#instafeed').slick({
+					slidesToShow: 4,
+					slidesToScroll: 1
+				});
+				let dateHidden = $(document).find('.instafeed-item-bot-dateHidden');
+				let dateShow = $(document).find('.instafeed-item-bot-date');
+				for (let i = 0; i < dateHidden.length; i++) {
+					dateShow.eq(i).text( timeConverter( dateHidden.eq(i).text() ) );
+				}
+				$(document).find('.main-inst-btn__link').attr( 'href', 'https://www.instagram.com/' + $(document).find('.instafeed-item-top-info__title').eq(0).text() );
+			}
+    });
+},
+initialSlickElement = document.querySelectorAll('#instafeed')[0],
+initialSlickObserver = new MutationObserver(initialSlick),
+initialSlickOptions = {
+    // обязательный параметр: наблюдаем за добавлением и удалением дочерних элементов.
+    'childList': true,
+    // наблюдаем за добавлением и удалением дочерних элементов любого уровня вложенности.
+    'subtree': true
+}
+initialSlickObserver.observe(initialSlickElement, initialSlickOptions);
 
 	resizewindow();
 	$(window).resize(function(e){
